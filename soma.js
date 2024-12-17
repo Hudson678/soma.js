6a0c220956a76802f32a13651321f4dbdf209e7f
Hudson Moraes <Hudsonmoraes31@gmail.com>
2024-12-17 20:19:27 -0300

    soma no javascript

---
+++ soma.js
@@ -0,0 +1,12 @@
+// Função para somar dois números
+function soma(a, b) {
+    return a + b;
+}
+
+// Exemplo de uso
+let numero1 = 5; // Primeiro número
+let numero2 = 3; // Segundo número
+
+let resultado = soma(numero1, numero2);
+
+console.log("A soma é: " + resultado);
